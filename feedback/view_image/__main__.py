bc454a154000cf3eda8ca307a6c15e2d2209ef36
Kennedy Richard <kennedy@kennedyrichard.com>
2022-07-16 17:03:48 -0300

    view_image handles window resizing and scrolling

--- feedback/view_image/__main__.py
+++ feedback/view_image/__main__.py
@@ -8,14 +8,28 @@ from itertools import cycle
 
 ## pygame
 
-from pygame import QUIT, KEYUP, K_ESCAPE, Surface, Rect
+from pygame import (
+
+              QUIT,
+
+              KEYUP,
+
+              K_ESCAPE,
+
+              K_w, K_a, K_s, K_d,
+              K_UP, K_LEFT, K_DOWN, K_RIGHT,
+
+              Surface, Rect,
+
+            )
 
 from pygame.display import get_surface, update
 from pygame.time    import Clock
-from pygame.math    import Vector2
 from pygame.event   import get as get_events
 from pygame.image   import fromstring as image_from_string
 from pygame.draw    import rect as draw_rect
+
+from pygame.key import get_pressed as get_pressed_keys
 
 
 ## Pillow
@@ -25,19 +39,23 @@ from PIL.Image import Image
 
 ### get screen and its rect
 
-screen      = get_surface()
-screen_rect = screen.get_rect()
+SCREEN      = get_surface()
+SCREEN_RECT = SCREEN.get_rect()
 
-### get a vector for the center of the screen
-screen_center = Vector2(screen_rect.center)
+### create a scroll area so the image can be moved around
+SCROLL_AREA = SCREEN_RECT.inflate(-80, -80)
+
+### define scrolling speeds in different 2D axes
+
+X_SCROLLING_SPEED = 20
+Y_SCROLLING_SPEED = 20
 
 ### instantiate background
-background = Surface(screen.get_size()).convert()
+BACKGROUND = Surface(SCREEN.get_size()).convert()
 
-### create a flag to determine whether we must draw a
-### a checker pattern on the background (in case we didn't
-### do so yet)
-MUST_DRAW_CHECKER_PATTERN = [None]
+### create flag so drawing the checker pattern can be
+### postponed to when the node is executed
+MUST_DRAW_CHECKER_PATTERN = True
 
 
 def blit_checker_pattern(surf):
@@ -79,6 +97,7 @@ def blit_checker_pattern(surf):
         ## move it down using the y_offset
 
         if not surf_rect.colliderect(unit_rect):
+
             x_offset = -x_offset
             unit_rect.move_ip(x_offset, y_offset)
 
@@ -95,19 +114,176 @@ def blit_checker_pattern(surf):
         unit_rect.move_ip(x_offset, 0)
 
 
+### define function to manage scrolling the image
+
+def manage_scrolling(image_rect):
+
+    key_input = get_pressed_keys()
+
+    ### calculate x movement
+
+    if image_rect.width > SCREEN_RECT.width:
+
+        go_left = any(
+          key_input[key] for key in (K_a, K_LEFT)
+        )
+
+        go_right = any(
+          key_input[key] for key in (K_d, K_RIGHT)
+        )
+
+        if (
+
+             (go_left and go_right)
+          or (not go_left and not go_right)
+
+        ):
+            x_movement = 0
+
+        elif go_left and not go_right:
+            x_movement = -1 * X_SCROLLING_SPEED
+
+        elif go_right and not go_left:
+            x_movement = 1 * X_SCROLLING_SPEED
+
+    else: x_movement = 0
+
+    ### apply x movement if != 0
+
+    if x_movement < 0:
+
+        if (
+          (image_rect.right + x_movement)
+          < SCROLL_AREA.right
+        ):
+            image_rect.right = SCROLL_AREA.right
+
+        else: image_rect.x += x_movement
+
+    elif x_movement > 0:
+
+        if (
+          (image_rect.left + x_movement)
+          > SCROLL_AREA.left
+        ):
+            image_rect.left = SCROLL_AREA.left
+
+        else: image_rect.x += x_movement
+
+    ### calculate y movement
+
+    if image_rect.height > SCREEN_RECT.height:
+
+        go_up = any(
+          key_input[key] for key in (K_w, K_UP)
+        )
+
+        go_down = any(
+          key_input[key] for key in (K_s, K_DOWN)
+        )
+
+        if (
+
+             (go_up and go_down)
+          or (not go_up and not go_down)
+
+        ):
+            y_movement = 0
+
+        elif go_up and not go_down:
+            y_movement = -1 * Y_SCROLLING_SPEED
+
+        elif go_down and not go_up:
+            y_movement = 1 * Y_SCROLLING_SPEED
+
+    else: y_movement = 0
+
+    ### apply y movement if != 0
+
+    if y_movement < 0:
+
+        if (
+          (image_rect.bottom + y_movement)
+          < SCROLL_AREA.bottom
+        ):
+            image_rect.bottom = SCROLL_AREA.bottom
+
+        else: image_rect.y += y_movement
+
+    elif y_movement > 0:
+
+        if (
+          (image_rect.top + y_movement)
+          > SCROLL_AREA.top
+        ):
+            image_rect.top = SCROLL_AREA.top
+
+        else: image_rect.y += y_movement
+
+
+### define function to watch out for window resizing
+### and perform needed setups when it is the case
+
+def watch_window_size(image_surf, image_rect):
+
+    global BACKGROUND
+
+    ### if the screen and the background have the
+    ### same size, then no window resizing took place,
+    ### so we exit the function right away
+    if SCREEN.get_size() == BACKGROUND.get_size(): return
+
+    ### other, we keep executing the function, performing
+    ### the window resize setups
+
+    ## update the screen rect's size
+    SCREEN_RECT.size = SCREEN.get_size()
+
+    ## center the image on the screen
+    image_rect.center = SCREEN_RECT.center
+
+    ## recreate the background
+
+    BACKGROUND = (
+
+      Surface(SCREEN.get_size()).convert()
+
+    )
+
+    ## redraw the checker pattern on the background
+    blit_checker_pattern(BACKGROUND)
+
+    ## clean the screen
+    SCREEN.blit(BACKGROUND, (0, 0))
+
+    ## blit image on the screen using its rect
+    SCREEN.blit(image_surf, image_rect)
+
+    ## replace the scroll area
+
+    global SCROLL_AREA
+    SCROLL_AREA = SCREEN_RECT.inflate(-80, -80)
+
+
 def view_image(image: Image):
     """Display image centered on screen.
 
     To stop displaying the image just press <Escape>.
     This will trigger the exit of the inner loop.
     """
-    ### if check pattern wasn't draw yet on background,
-    ### do so now
+    ### draw the checker pattern on the background if
+    ### needed
+
+    global MUST_DRAW_CHECKER_PATTERN
 
     if MUST_DRAW_CHECKER_PATTERN:
 
-        blit_checker_pattern(background)
-        MUST_DRAW_CHECKER_PATTERN.pop()
+        ### draw the checker pattern on the background
+        blit_checker_pattern(BACKGROUND)
+
+        ### set flag to false
+        MUST_DRAW_CHECKER_PATTERN = False
+
 
     ### obtain surface from pillow image
 
@@ -118,15 +294,19 @@ def view_image(image: Image):
     surf = image_from_string(data, size, mode)
 
     ### clean the screen
-    screen.blit(background, (0, 0))
+    SCREEN.blit(BACKGROUND, (0, 0))
 
     ### get rect for image and center it on the screen
 
-    rect        = surf.get_rect()
-    rect.center = screen_center
+    rect = surf.get_rect()
 
-    ### blit image center on the screen using the rect
-    screen.blit(surf, rect)
+    rect.center = SCREEN_RECT.center
+
+    ### blit image on the screen using its rect
+    SCREEN.blit(surf, rect)
+
+    ### create variable to track topleft position
+    last_topleft = rect.topleft
 
     ### timing support
 
@@ -138,14 +318,34 @@ def view_image(image: Image):
     while running:
 
         clock.tick(30)
+
+        watch_window_size(surf, rect)
         
+        ### handle events
+
         for event in get_events():
 
             if event.type == QUIT: running = False
 
             elif event.type == KEYUP:
+
                 if event.key == K_ESCAPE:
                     running = False
+
+        ### manage scrolling
+        manage_scrolling(rect)
+
+        ### if image moved, redraw
+
+        if last_topleft != rect.topleft:
+
+            last_topleft = rect.topleft
+
+            ### clean the screen
+            SCREEN.blit(BACKGROUND, (0, 0))
+
+            ### blit image on the screen using its rect
+            SCREEN.blit(surf, rect)
 
         ### update screen
         update()
