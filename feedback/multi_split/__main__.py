2ea6fc6f982b521fa30568af98e5122d4799a0d7
Kennedy Richard <kennedy@kennedyrichard.com>
2022-07-19 16:59:20 -0300

    state handling & mouse dragging for multi_split

--- feedback/multi_split/__main__.py
+++ feedback/multi_split/__main__.py
@@ -8,14 +8,34 @@ from itertools import cycle
 
 ## pygame
 
-from pygame import QUIT, KEYUP, K_ESCAPE, Surface, Rect
+from pygame import (
+
+              QUIT,
+
+              KEYUP,
+
+              K_ESCAPE,
+
+
+              K_w, K_a, K_s, K_d,
+              K_UP, K_LEFT, K_DOWN, K_RIGHT,
+              K_HOME,
+
+              MOUSEBUTTONUP,
+              MOUSEBUTTONDOWN,
+              MOUSEMOTION,
+
+              Surface, Rect,
+
+            )
 
 from pygame.display import get_surface, update
 from pygame.time    import Clock
-from pygame.math    import Vector2
 from pygame.event   import get as get_events
 from pygame.image   import fromstring as image_from_string
 from pygame.draw    import rect as draw_rect
+
+from pygame.key import get_pressed as get_pressed_keys
 
 
 ## Pillow
@@ -24,25 +44,462 @@ from PIL.Image import Image
 
 ### get screen and its rect
 
-screen      = get_surface()
-screen_rect = screen.get_rect()
+SCREEN      = get_surface()
+SCREEN_RECT = SCREEN.get_rect()
 
-### get a vector for the center of the screen
-screen_center = Vector2(screen_rect.center)
+### define scrolling speeds in different 2D axes
 
-### instantiate background
-background = Surface(screen.get_size()).convert()
+X_SCROLLING_SPEED = 20
+Y_SCROLLING_SPEED = 20
 
 ### obtain fps maintaining operation
 maintain_fps = Clock().tick
 
-### create a flag to determine whether we must draw a
-### a checker pattern on the background (in case we didn't
-### do so yet)
-MUST_DRAW_CHECKER_PATTERN = [None]
+
+class ImageViewer:
+    """Manages the loop of the view_image() node."""
+
+    def __init__(self):
+
+        ### create a scroll area so the image can be moved
+        ### around
+        self.scroll_area = SCREEN_RECT.inflate(-80, -80)
+
+        ### instantiate background
+
+        self.background = (
+          Surface(SCREEN.get_size()).convert()
+        )
+
+        ### create flag so drawing the checker pattern can
+        ### be postponed to when the node is executed
+        self.must_draw_checker_pattern = True
+
+    def keyboard_mode_event_handling(self):
+
+        for event in get_events():
+
+            if event.type == QUIT:
+                self.running = False
+
+            elif event.type == MOUSEBUTTONDOWN:
+
+                if event.button == 1:
+                    self.enable_mouse_mode()
+
+            elif event.type == KEYUP:
+
+                if event.key == K_HOME:
+
+                    self.image_rect.center = (
+                      SCREEN_RECT.center
+                    )
+
+                elif event.key == K_ESCAPE:
+                    self.running = False
+
+    def keyboard_mode_key_state_handling(self):
+        """Handle the state of keys."""
+
+        key_input = get_pressed_keys()
+
+        ### calculate x movement
+
+        if self.moves_horizontally:
+
+            go_left = any(
+              key_input[key] for key in (K_a, K_LEFT)
+            )
+
+            go_right = any(
+              key_input[key] for key in (K_d, K_RIGHT)
+            )
+
+            if go_left and not go_right:
+                dx = -1 * X_SCROLLING_SPEED
+
+            elif go_right and not go_left:
+                dx = 1 * X_SCROLLING_SPEED
+
+            else: dx = 0
+
+        else: dx = 0
+
+        ### calculate y movement
+
+        if self.moves_vertically:
+
+            go_up = any(
+              key_input[key] for key in (K_w, K_UP)
+            )
+
+            go_down = any(
+              key_input[key] for key in (K_s, K_DOWN)
+            )
+
+            if (
+
+                 (go_up and go_down)
+              or (not go_up and not go_down)
+
+            ):
+                dy = 0
+
+            elif go_up and not go_down:
+                dy = -1 * Y_SCROLLING_SPEED
+
+            elif go_down and not go_up:
+                dy = 1 * Y_SCROLLING_SPEED
+
+        else: dy = 0
+
+        ### apply movement if a delta is found
+        if dx or dy: self.move_image(dx, dy)
+
+    def move_image(self, dx, dy):
+
+        image_rect  = self.image_rect
+        scroll_area = self.scroll_area
+
+        ### apply x movement if != 0
+
+        if dx < 0:
+
+            if (
+              (image_rect.right + dx)
+              < scroll_area.right
+            ):
+                image_rect.right = scroll_area.right
+
+            else: image_rect.x += dx
+
+        elif dx > 0:
+
+            if (
+              (image_rect.left + dx)
+              > scroll_area.left
+            ):
+                image_rect.left = scroll_area.left
+
+            else: image_rect.x += dx
+
+        ### apply y movement if != 0
+
+        if dy < 0:
+
+            if (
+              (image_rect.bottom + dy)
+              < scroll_area.bottom
+            ):
+                image_rect.bottom = scroll_area.bottom
+
+            else: image_rect.y += dy
+
+        elif dy > 0:
+
+            if (
+              (image_rect.top + dy)
+              > scroll_area.top
+            ):
+                image_rect.top = scroll_area.top
+
+            else: image_rect.y += dy
+
+    def mouse_mode_event_handling(self):
+
+        for event in get_events():
+
+            if event.type == QUIT:
+                self.running = False
+
+            elif event.type == MOUSEMOTION:
+                self.move_according_to_mouse(*event.rel)
+
+            elif event.type == MOUSEBUTTONUP:
+
+                if event.button == 1:
+                    self.enable_keyboard_mode()
+
+            elif event.type == KEYUP:
+
+                if event.key == K_ESCAPE:
+                    self.running = False
+
+    def move_according_to_mouse(self, dx, dy):
+
+        if not self.moves_horizontally:
+            dx = 0
+
+        if not self.moves_vertically:
+            dy = 0
+
+        self.move_image(dx, dy)
+
+    def mouse_mode_key_state_handling(self):
+        """Do nothing."""
+
+    def watch_window_size(self):
+        """Watch out for window resizing.
+
+        And perform needed setups when it is the case
+        """
+
+        ### if the screen and the background have the
+        ### same size, then no window resizing took place,
+        ### so we exit the function right away
+
+        if SCREEN.get_size() == self.background.get_size():
+            return
+
+        ### other, we keep executing the function,
+        ### performing the window resize setups
+
+        ## reference image surf and rect locally
+
+        image_surf = self.image_surf
+        image_rect = self.image_rect
+
+        ## update the screen rect's size
+        SCREEN_RECT.size = SCREEN.get_size()
+
+        ## center the image on the screen
+        image_rect.center = SCREEN_RECT.center
+
+        ## update the moving flags
+
+        self.moves_horizontally = (
+          image_rect.width > SCREEN_RECT.width
+        )
+
+        self.moves_vertically = (
+          image_rect.height > SCREEN_RECT.height
+        )
+
+        ## recreate the background
+
+        self.background = (
+
+          Surface(SCREEN.get_size()).convert()
+
+        )
+
+        ## redraw the checker pattern on the background
+        blit_checker_pattern(self.background)
+
+        ## clean the screen
+        SCREEN.blit(self.background, (0, 0))
+
+        ## blit image on the screen using its rect
+        SCREEN.blit(image_surf, image_rect)
+
+        ## replace the scroll area
+        self.scroll_area = SCREEN_RECT.inflate(-80, -80)
+
+    def enable_keyboard_mode(self):
+        """Set behaviour to move image with keyboard."""
+
+        self.handle_events = (
+          self.keyboard_mode_event_handling
+        )
+
+        self.handle_key_state = (
+          self.keyboard_mode_key_state_handling
+        )
+
+    def enable_mouse_mode(self):
+        """Set behaviour to move image with the mouse.
+
+        That is, by dragging.
+        """
+
+        self.handle_events = (
+          self.mouse_mode_event_handling
+        )
+
+        self.handle_key_state = (
+          self.mouse_mode_key_state_handling
+        )
+
+    def draw(self):
+        """If image moved, redraw."""
+
+        ### if the image is in the same position,
+        ### do nothing by returning early
+
+        if (
+          self.last_topleft == self.image_rect.topleft
+        ): return
+
+        ### otherwise store the current position and
+        ### redraw background and image
+
+        self.last_topleft = self.image_rect.topleft
+
+        SCREEN.blit(self.background, (0, 0))
+
+        SCREEN.blit(
+                 self.image_surf, self.image_rect
+               )
+
+    def multi_split_view(
+
+          self,
+
+          *images: Image,
+
+          axis_to_split: {
+            'widget_name': 'option_tray',
+            'widget_kwargs': {
+              'options': ['X', 'Y'],
+            },
+            'type': str,
+          } = 'X',
+
+        ):
+        """Display image centered on screen, then return it.
+
+        To stop displaying the image just press <Escape>.
+        This will trigger the exit of the inner loop.
+        """
+        ### raise error if no images are given
+
+        if not images:
+
+            raise ValueError(
+                    "No images were received."
+                  )
+
+        ### enable keyboard mode
+        self.enable_keyboard_mode()
+
+        ### draw the checker pattern on the background if
+        ### needed
+
+        if self.must_draw_checker_pattern:
+
+            ### draw the checker pattern on the background
+            blit_checker_pattern(self.background)
+
+            ### set flag to false
+            self.must_draw_checker_pattern = False
+
+        ### obtain surfaces from pillow images
+
+        surfs = [
+          surf_from_pillow_image(image)
+          for image in images
+        ]
+
+        n = len(surfs)
+
+        rect   = surfs[0].get_rect()
+        canvas = Surface(rect.size).convert()
+
+        (
+          dimension_to_split,
+          index_to_increment,
+        ) = (
+
+          (
+            'height',
+            1,
+          )
+          if axis_to_split.lower() == 'y'
+
+          else (
+            'width',
+            0,
+          )
+
+        )
+
+        value = getattr(rect, dimension_to_split)
+        pixels_per_split, remainder = divmod(value, n)
+
+        split_values = [pixels_per_split] * n
+        split_values[-1] += remainder
+
+        split_rect = rect.copy()
+
+        for surf, split_value in (
+          zip(surfs, split_values)
+        ):
+            
+            setattr(
+              split_rect,
+              dimension_to_split,
+              split_value
+            )
+
+            canvas.blit(
+                     surf.subsurface(split_rect),
+                     split_rect,
+                   )
+
+            split_rect[index_to_increment] += split_value
+
+        ### clean the screen
+        SCREEN.blit(self.background, (0, 0))
+
+        ### center image rect on the screen
+        rect.center = SCREEN_RECT.center
+
+        ### update the moving flags
+
+        self.moves_horizontally = (
+          rect.width > SCREEN_RECT.width
+        )
+
+        self.moves_vertically = (
+          rect.height > SCREEN_RECT.height
+        )
+
+        ### blit image center on the screen using the rect
+        SCREEN.blit(canvas, rect)
+
+        ### create attribute to track topleft position
+        self.last_topleft = rect.topleft
+
+        ### store image surface and rect
+
+        self.image_surf = canvas
+        self.image_rect = rect
+
+        ### loop
+        self.loop()
+
+        ### remove image surf and rect references
+
+        del self.image_surf
+        del self.image_rect
+
+    multi_split_view.dismiss_exec_time_tracking = True
+
+    def loop(self):
+
+        self.running = True
+
+        while self.running:
+
+            maintain_fps(30)
+
+            self.watch_window_size()
+
+            self.handle_events()
+            self.handle_key_state()
+            self.draw()
+
+            ### update screen (pygame.display.update())
+            update()
 
 
-def _blit_checker_pattern(surf):
+
+main_callable = ImageViewer().multi_split_view
+
+
+### utility functions
+
+def blit_checker_pattern(surf):
     """Blit checker pattern on surf with colors and rect."""
     ### define settings
 
@@ -97,7 +554,7 @@ def _blit_checker_pattern(surf):
         unit_rect.move_ip(x_offset, 0)
 
 
-def _surf_from_pillow_image(image):
+def surf_from_pillow_image(image):
 
     mode = image.mode
     size = image.size
@@ -105,132 +562,3 @@ def _surf_from_pillow_image(image):
 
     return image_from_string(data, size, mode)
 
-
-def multi_split_view(
-
-      *images: Image,
-
-      axis_to_split: {
-        'widget_name': 'option_tray',
-        'widget_kwargs': {
-          'options': ['X', 'Y'],
-        },
-        'type': str,
-      } = 'X',
-
-    ):
-    """Display image centered on screen, then return it.
-
-    To stop displaying the image just press <Escape>.
-    This will trigger the exit of the inner loop.
-    """
-    ### raise error if no images are given
-
-    if not images:
-
-        raise ValueError(
-                "No images were received."
-              )
-
-    ### if check pattern wasn't draw yet on background,
-    ### do so now
-
-    if MUST_DRAW_CHECKER_PATTERN:
-
-        _blit_checker_pattern(background)
-        MUST_DRAW_CHECKER_PATTERN.pop()
-
-    ### obtain surfaces from pillow images
-
-    surfs = [
-      _surf_from_pillow_image(image)
-      for image in images
-    ]
-
-    n = len(surfs)
-
-    rect   = surfs[0].get_rect()
-    canvas = Surface(rect.size).convert()
-
-    (
-      dimension_to_split,
-      index_to_increment,
-    ) = (
-
-      (
-        'height',
-        1,
-      )
-      if axis_to_split.lower() == 'y'
-
-      else (
-        'width',
-        0,
-      )
-
-    )
-
-    value = getattr(rect, dimension_to_split)
-    pixels_per_split, remainder = divmod(value, n)
-
-    split_values = [pixels_per_split] * n
-    split_values[-1] += remainder
-
-    split_rect = rect.copy()
-
-    for surf, split_value in (
-      zip(surfs, split_values)
-    ):
-        
-        setattr(
-          split_rect,
-          dimension_to_split,
-          split_value
-        )
-
-        canvas.blit(
-                 surf.subsurface(split_rect),
-                 split_rect,
-               )
-
-        split_rect[index_to_increment] += split_value
-
-    ### clean the screen
-    screen.blit(background, (0, 0))
-
-    ### center image rect on the screen
-    rect.center = screen_center
-
-    ### blit image center on the screen using the rect
-    screen.blit(canvas, rect)
-
-    ### loop support
-
-    running = True
-
-    ### start loop
-
-    while running:
-
-        maintain_fps(30)
-        
-        for event in get_events():
-
-            if event.type == QUIT: running = False
-
-            elif (
-
-                  event.type == KEYUP
-              and event.key  == K_ESCAPE
-
-            ):
-
-                running = False
-
-        ### update screen
-        update()
-
-
-multi_split_view.dismiss_exec_time_tracking = True
-
-main_callable = multi_split_view
