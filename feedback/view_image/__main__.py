5b61c01b700977169aaedf3f333f0ae8f0568ce4
Kennedy Richard <kennedy@kennedyrichard.com>
2022-07-22 11:49:56 -0300

    Improved comments and docstrings on view_image

--- feedback/view_image/__main__.py
+++ feedback/view_image/__main__.py
@@ -56,11 +56,15 @@ Y_SCROLLING_SPEED = 20
 ### obtain fps maintaining operation
 maintain_fps = Clock().tick
 
+### define the framerate
+FPS = 30
+
 
 class ImageViewer:
     """Manages the loop of the view_image() node."""
 
     def __init__(self):
+        """Create support objects/flags."""
 
         ### create a scroll area so the image can be moved
         ### around
@@ -72,11 +76,31 @@ class ImageViewer:
           Surface(SCREEN.get_size()).convert()
         )
 
-        ### create flag so drawing the checker pattern can
-        ### be postponed to when the node is executed
+        ### create flag to indicate whether the checker
+        ### pattern must be drawn on the background;
+        ###
+        ### the first time the node is executed the
+        ### checker pattern is draw on the background
+        ### and then this flag is set to False; it will
+        ### remain False for the lifetime of the node,
+        ### that is, it is used only that time;
+        ###
+        ### it is actually optional: we could draw the
+        ### checker pattern right away if we wanted; we
+        ### just avoid doing so because though it happens
+        ### practically in an instant, there's no telling
+        ### whether other nodes will also perform setups
+        ### that take time when the node pack is loaded
+        ### and the sum of the time taken by all nodes
+        ### might end up resulting in a non-trivial amount
+        ### of time to load the node pack;
+        ###
+        ### maybe we are just being overly cautious,
+        ### though;
         self.must_draw_checker_pattern = True
 
     def keyboard_mode_event_handling(self):
+        """Event handling for the keyboard mode."""
 
         for event in get_events():
 
@@ -100,13 +124,19 @@ class ImageViewer:
                     self.running = False
 
     def keyboard_mode_key_state_handling(self):
-        """Handle the state of keys."""
+        """Handle pressed keys for keyboard mode."""
 
         key_input = get_pressed_keys()
 
         ### calculate x movement
 
+        ## calculate x movement if the "moves_horizontally"
+        ## flag is set
+
         if self.moves_horizontally:
+
+            ## check whether "go left" and "go right"
+            ## buttons were pressed
 
             go_left = any(
               key_input[key] for key in (K_a, K_LEFT)
@@ -116,6 +146,10 @@ class ImageViewer:
               key_input[key] for key in (K_d, K_RIGHT)
             )
 
+            ## assign amount of movement on x axis
+            ## depending on whether "go left" and "go right"
+            ## buttons were pressed
+
             if go_left and not go_right:
                 dx = -1 * X_SCROLLING_SPEED
 
@@ -124,9 +158,15 @@ class ImageViewer:
 
             else: dx = 0
 
+        ## if the "moves_horizontally" flag is not set,
+        ## it means the image's width is smaller than
+        ## the screen's width, so there's no need to
+        ## move/scroll horizontally anyway, so the
+        ## movement is 0
         else: dx = 0
 
-        ### calculate y movement
+        ### perform the same checks/calculations for
+        ### the y axis
 
         if self.moves_vertically:
 
@@ -154,10 +194,17 @@ class ImageViewer:
 
         else: dy = 0
 
-        ### apply movement if a delta is found
+        ### if there is movement in the x or y
+        ### axis, move the image
         if dx or dy: self.move_image(dx, dy)
 
     def move_image(self, dx, dy):
+        """Move image in x and/or y axis.
+
+        If performs extra checks/movement relative to
+        a scroll area to ensure the image never leaves
+        the screen completely.
+        """
 
         image_rect  = self.image_rect
         scroll_area = self.scroll_area
@@ -207,6 +254,7 @@ class ImageViewer:
             else: image_rect.y += dy
 
     def mouse_mode_event_handling(self):
+        """Event handling for the mouse mode."""
 
         for event in get_events():
 
@@ -227,6 +275,7 @@ class ImageViewer:
                     self.running = False
 
     def move_according_to_mouse(self, dx, dy):
+        """Support method for the mouse mode."""
 
         if not self.moves_horizontally:
             dx = 0
@@ -237,13 +286,13 @@ class ImageViewer:
         self.move_image(dx, dy)
 
     def mouse_mode_key_state_handling(self):
-        """Do nothing."""
+        """Mouse mode doesn't handle key pressed state.
+
+        So this method does nothing.
+        """
 
     def watch_window_size(self):
-        """Watch out for window resizing.
-
-        And perform needed setups when it is the case
-        """
+        """Perform setups if window was resized."""
 
         ### if the screen and the background have the
         ### same size, then no window resizing took place,
@@ -252,8 +301,8 @@ class ImageViewer:
         if SCREEN.get_size() == self.background.get_size():
             return
 
-        ### other, we keep executing the function,
-        ### performing the window resize setups
+        ### otherwise, we keep executing the function,
+        ### performing the needed setups
 
         ## reference image surf and rect locally
 
@@ -276,7 +325,8 @@ class ImageViewer:
           image_rect.height > SCREEN_RECT.height
         )
 
-        ## recreate the background
+        ## recreate the background and redraw the checker
+        ## pattern on it
 
         self.background = (
 
@@ -284,10 +334,9 @@ class ImageViewer:
 
         )
 
-        ## redraw the checker pattern on the background
         blit_checker_pattern(self.background)
 
-        ## clean the screen
+        ## draw the background on the screen
         SCREEN.blit(self.background, (0, 0))
 
         ## blit image on the screen using its rect
@@ -296,95 +345,8 @@ class ImageViewer:
         ## replace the scroll area
         self.scroll_area = SCREEN_RECT.inflate(-80, -80)
 
-    def view_image(self, image: Image):
-        """Display image centered on screen.
-
-        To stop displaying the image just press <Escape>.
-        This will trigger the exit of the inner loop.
-        """
-        ### enable keyboard mode
-        self.enable_keyboard_mode()
-
-        ### draw the checker pattern on the background if
-        ### needed
-
-        if self.must_draw_checker_pattern:
-
-            ### draw the checker pattern on the background
-            blit_checker_pattern(self.background)
-
-            ### set flag to false
-            self.must_draw_checker_pattern = False
-
-
-        ### obtain surface from pillow image
-
-        mode = image.mode
-        size = image.size
-        data = image.tobytes()
-
-        surf = image_from_string(data, size, mode)
-
-        ### clean the screen
-        SCREEN.blit(self.background, (0, 0))
-
-        ### get rect for image and center it on the screen
-
-        rect = surf.get_rect()
-
-        rect.center = SCREEN_RECT.center
-
-        ### update the moving flags
-
-        self.moves_horizontally = (
-          rect.width > SCREEN_RECT.width
-        )
-
-        self.moves_vertically = (
-          rect.height > SCREEN_RECT.height
-        )
-
-        ### store image surface and rect
-
-        self.image_surf = surf
-        self.image_rect = rect
-
-        ### blit image on the screen using its rect
-        SCREEN.blit(surf, rect)
-
-        ### create attribute to track topleft position
-        self.last_topleft = rect.topleft
-
-        ### loop
-        self.loop()
-
-        ### remove image surf and rect references
-
-        del self.image_surf
-        del self.image_rect
-
-    ### set special attribute on view_image method
-    view_image.dismiss_exec_time_tracking = True
-
-    def loop(self):
-
-        self.running = True
-
-        while self.running:
-
-            maintain_fps(30)
-
-            self.watch_window_size()
-
-            self.handle_events()
-            self.handle_key_state()
-            self.draw()
-
-            ### update screen (pygame.display.update())
-            update()
-
     def enable_keyboard_mode(self):
-        """Set behaviour to move image with keyboard."""
+        """Set behaviours to move image with keyboard."""
 
         self.handle_events = (
           self.keyboard_mode_event_handling
@@ -395,7 +357,7 @@ class ImageViewer:
         )
 
     def enable_mouse_mode(self):
-        """Set behaviour to move image with the mouse.
+        """Set behaviours to move image with the mouse.
 
         That is, by dragging.
         """
@@ -429,14 +391,148 @@ class ImageViewer:
                  self.image_surf, self.image_rect
                )
 
+    def loop(self):
+        """Start and keep a loop.
 
+        The loop is only exited when the running flag
+        is set to False.
+        """
+
+        self.running = True
+
+        while self.running:
+
+            ## maintain a constant fps
+            maintain_fps(FPS)
+
+            ## watch out for change in the window size,
+            ## performing needed setups if such change
+            ## happened
+            self.watch_window_size()
+
+            ## execute main operation of the loop,
+            ## that is, input handling and drawing
+
+            self.handle_events()
+            self.handle_key_state()
+            self.draw()
+
+            ## finally update the screen with
+            ## pygame.display.update()
+            update()
+
+    ### the method below is the main callable we'll use
+    ### for our node;
+    ###
+    ### that is, we'll instantiate the ImageViewer class
+    ### and use this method from the instance as the
+    ### main callable;
+    ###
+    ### don't worry about the "self" parameter, Nodezator
+    ### is smart enough to ignore it (actually, the smart
+    ### one is inspect.signature(), the responsible for
+    ### such behaviour)
+
+    def view_image(self, image: Image):
+        """Display image on screen.
+
+        To stop displaying the image just press <Escape>.
+        This will trigger the exit of the inner loop.
+        """
+        ### enable keyboard mode
+        self.enable_keyboard_mode()
+
+        ### draw the checker pattern on the background if
+        ### needed; this flag is only used once for the
+        ### lifetime of this node (check the comment on
+        ### the __init__ method about this flag)
+
+        if self.must_draw_checker_pattern:
+
+            ### draw the checker pattern on the background
+            blit_checker_pattern(self.background)
+
+            ### set flag to false
+            self.must_draw_checker_pattern = False
+
+
+        ### obtain surface from pillow image
+
+        mode = image.mode
+        size = image.size
+        data = image.tobytes()
+
+        surf = image_from_string(data, size, mode)
+
+        ### draw the background on the screen
+        SCREEN.blit(self.background, (0, 0))
+
+        ### get rect for image and center it on the screen
+
+        rect = surf.get_rect()
+
+        rect.center = SCREEN_RECT.center
+
+        ### update the moving flags;
+        ###
+        ### such flags just indicate whether moving the
+        ### image makes sense horizontally and
+        ### vertically, depending on whether the image
+        ### is larger than the screen or not;
+        ###
+        ### for instance, if the screen is wider than
+        ### the image, then there is no need to move
+        ### the image horizontally, so the corresponding
+        ### flag is set to false
+
+        self.moves_horizontally = (
+          rect.width > SCREEN_RECT.width
+        )
+
+        self.moves_vertically = (
+          rect.height > SCREEN_RECT.height
+        )
+
+        ### store image surface and rect
+
+        self.image_surf = surf
+        self.image_rect = rect
+
+        ### blit image on the screen using its rect
+        SCREEN.blit(surf, rect)
+
+        ### create attribute to track topleft position
+        self.last_topleft = rect.topleft
+
+        ### loop
+        self.loop()
+
+        ### remove image surf and rect references
+
+        del self.image_surf
+        del self.image_rect
+
+    ### set attribute on view_image method so the
+    ### execution time tracking is dismissed for this
+    ### node;
+    ###
+    ### we need to do this here rather than after
+    ### instantiating ImageViewer because after
+    ### instantiating the class the view_image method
+    ### can't have new attributes set on it
+    view_image.dismiss_exec_time_tracking = True
+
+
+### finally, we just need to instantiate the ImageViewer
+### and use the view_image method as the main callable
 main_callable = ImageViewer().view_image
 
 
-### utility function
+### the support function below is used to draw a checker
+### pattern on the background whenever needed
 
 def blit_checker_pattern(surf):
-    """Blit checker pattern on surf with colors and rect."""
+    """Blit checker pattern on surf."""
     ### define settings
 
     color_a = (235, 235, 235)
